f02ad6ee9d70feca1d5ed281c4ea30512e6f84ba
FAbdel <info@faris-abdelrehim.com>
2022-12-01 12:19:24 +0100

    Top three updated

--- 01_CalorieCounting/main copy.py
+++ 01_CalorieCounting/main copy.py
@@ -39,7 +39,7 @@ def get_calories_per_elve(filename):
     
     # Return the calories carried by each elve as list
     return calories_per_elve
-
+    
 def get_max_calories_and_elve(calories_per_elve):   
     """
     FUNCTION RETURNS MAX CALORIES AND THE ELVE WHO CARRIES IT
@@ -49,9 +49,40 @@ def get_max_calories_and_elve(calories_per_elve):
 
 def get_top_three_calories(calories_per_elve): 
     """
-    FUNCTION RETURNS THE SUM OF THE TOP THREE CALORIES 
+    FUNCTION RETURNS TOP THREE CALORIES IN TOTAL
     """    
-    return sum(sorted(calories_per_elve)[-3:])
+    
+    # Store complete calories list  
+    temp_list_calories = calories_per_elve
+    
+    # Init top three calorie values list
+    top_three_calories_total_list = []
+    
+    # Init current maximum 
+    current_maximum = 0
+
+    # Do as long as top three has not three elements
+    while len(top_three_calories_total_list) <3:
+        
+        # Get current maximum in list
+        current_maximum = max(temp_list_calories)
+        
+        # Get number of occurrences of maximum in list (in case there are more than one)
+        n_occurrences = temp_list_calories.count(current_maximum)
+        
+        for iOccurrences in range(0,n_occurrences):
+            # Append maximum for number of occurrences
+            top_three_calories_total_list.append(current_maximum)               
+        
+        # Remove current maximum from list  
+        temp_list_calories.remove(current_maximum)
+        
+        # Does top_three_calories_total_list containt 3 or more values? If yes, cut the remaining elements
+        if len(top_three_calories_total_list) >= 3:
+            top_three_calories_total_list = top_three_calories_total_list[0:3]
+
+    # Return sum of top three
+    return sum(top_three_calories_total_list)
     
 if __name__ == "__main__":
     # Specify calorie_list filename
