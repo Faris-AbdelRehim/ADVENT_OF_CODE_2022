6e78e2617b1cc34f2c67d4d04e5da9e24746e504
FAbdel <info@faris-abdelrehim.com>
2022-12-01 12:19:56 +0100

    Comments added

--- 01_CalorieCounting/main.py
+++ 01_CalorieCounting/main.py
@@ -51,6 +51,7 @@ def get_top_three_calories(calories_per_elve):
     """
     FUNCTION RETURNS THE SUM OF THE TOP THREE CALORIES 
     """    
+    
     return sum(sorted(calories_per_elve)[-3:])
     
 if __name__ == "__main__":
